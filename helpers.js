ae5983fae44fc4158fde3766b71e3a873d6ad582
sladynnunes <snunes@usc.edu>
2023-06-08 23:44:08 -0700

    Initial commit

---
+++ helpers.js
@@ -0,0 +1,107 @@
+/**
+ * Modified from https://github.com/austinEng/webgpu-samples/blob/master/src/helpers.ts
+ */
+
+ const helpers = {};
+
+ async function loadImages(srcList, needCors) {
+    const imageList = await Promise.all(srcList.map(async (src) => {
+         const img = new Image();
+         if (needCors) {
+             img.crossOrigin = 'anonymous'
+         }
+         img.src = src;
+         await img.decode();
+         return img;
+     }));
+     return imageList;
+ }
+ 
+ function createImageData(imageList) {
+     const imageCount = imageList.length;
+     let width;
+     let height;
+     let bytesPerRow;
+ 
+     const imageDataList = imageList.map(img => {
+         imageWidth = img.width;
+         imageHeight = img.height;
+ 
+         const imageCanvas = document.createElement('canvas');
+         imageCanvas.width = imageWidth;
+         imageCanvas.height = imageHeight;
+ 
+         const imageCanvasContext = imageCanvas.getContext('2d');
+         imageCanvasContext.translate(0, imageHeight);
+         imageCanvasContext.scale(1, -1);
+         imageCanvasContext.drawImage(img, 0, 0, imageWidth, imageHeight);
+         const imageData = imageCanvasContext.getImageData(0, 0, imageWidth, imageHeight);
+ 
+         bytesPerRow = Math.ceil(imageWidth * 4 / 256) * 256;
+         if (bytesPerRow == imageWidth * 4) {
+             return imageData.data;
+         } else {
+             const data = new Uint8Array(bytesPerRow * imageHeight);
+             let imagePixelIndex = 0;
+             for (let y = 0; y < img.height; ++y) {
+                 for (let x = 0; x < imageWidth; ++x) {
+                     let i = x * 4 + y * bytesPerRow;
+                     data[i] = imageData.data[imagePixelIndex];
+                     data[i + 1] = imageData.data[imagePixelIndex + 1];
+                     data[i + 2] = imageData.data[imagePixelIndex + 2];
+                     data[i + 3] = imageData.data[imagePixelIndex + 3];
+                     imagePixelIndex += 4;
+                 }
+             }
+ 
+             return data;
+         }
+     });
+ 
+     return {
+         imageDataList,
+         imageCount,
+         imageWidth,
+         imageHeight,
+         bytesPerRow,
+     }
+ };
+ 
+ helpers.createTextureFromImage = async function createTextureFromImage(device, src, usage, needCors) {
+     const srcList = Array.isArray(src)? src : [src];
+     const images = await loadImages(srcList, needCors);
+     const {
+         imageDataList,
+         imageCount,
+         imageWidth,
+         imageHeight,
+         bytesPerRow,
+     } = createImageData(images);
+ 
+     const texture = device.createTexture({
+         size: {
+             width: imageWidth,
+             height: imageHeight,
+             depthOrArrayLayers: imageCount,
+         },
+         dimension: '2d',
+         format: "rgba8unorm",
+         usage: GPUTextureUsage.COPY_DST | usage,
+     });
+ 
+     for(let i = 0;i < imageCount;i ++) {
+         device.queue.writeTexture({
+             texture,
+             origin:{
+                 z: i,
+             }
+         }, imageDataList[i], {
+             bytesPerRow,
+         }, {
+             width: imageWidth,
+             height: imageHeight,
+             depth: 1,
+         });
+     }
+     return texture;
+ };
---
+++ main.js
@@ -0,0 +1,168 @@
+async function initWebGPU() {
+    
+    const canvas = document.querySelector('#canvas');
+    canvas.width = canvas.height = 500;
+    canvas.style = 'width:500px;height:500px';
+    const adapter = await navigator.gpu.requestAdapter();
+    const device = await adapter.requestDevice();
+
+
+    const vs = `
+        struct VertexOutput {
+        @builtin(position) position : vec4<f32>,
+        @location(0) v_color : vec3<f32>,
+        @location(1) v_uv : vec2<f32>
+        };
+
+        @vertex
+        fn main(@location(0) a_position : vec2<f32>, 
+            @location(1) a_color : vec3<f32>,
+            @location(2) a_uv : vec2<f32>) -> VertexOutput {
+        var output : VertexOutput;
+        output.position = vec4<f32>(a_position, 0.0, 1.0);
+        output.v_color = a_color;
+        output.v_uv = a_uv;
+        return output;
+        }
+    `;
+
+    const fs = `
+        @group(0) @binding(0) var u_sampler: sampler;
+        @group(0) @binding(1) var u_texture: texture_2d<f32>;
+
+        @fragment
+        fn main(@location(0) v_color: vec3<f32>,
+            @location(1) v_uv: vec2<f32>) -> @location(0) vec4<f32> {
+        var fragColor = textureSample(u_texture, u_sampler, v_uv);
+        fragColor = fragColor * vec4<f32>(v_color, 1.0);
+        return fragColor;
+        }
+    `;
+
+    const context = canvas.getContext('webgpu');
+
+    const swapChainFormat = navigator.gpu.getPreferredCanvasFormat();
+
+    const swapChain = context.configure({
+        device,
+        format: swapChainFormat,
+        alphaMode: "opaque",
+    });
+
+    const verticesData = new Float32Array([
+        //position      color   uv
+        -0.8, -0.8, 1,  0, 0,   0, 0,
+        -0.8, 0.8, 0,   1, 0,   0, 1,
+        0.8, -0.8, 1,   0, 1,   1, 0,
+        0.8, 0.8, 0,    0, 1,   1, 1
+    ]);
+    const verticesBuffer = device.createBuffer({
+        size: verticesData.byteLength,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+        mappedAtCreation: true
+    });
+    new Float32Array(verticesBuffer.getMappedRange()).set(verticesData);
+    verticesBuffer.unmap();
+
+    const texture = await helpers.createTextureFromImage(device, './wallpaper.jpeg', GPUTextureUsage.TEXTURE_BINDING);
+    const sampler = device.createSampler({
+        magFilter: 'linear',
+        minFilter: 'linear',
+    });
+
+    const bindGroupLayout = device.createBindGroupLayout({
+        entries: [{
+            binding: 0,
+            visibility: GPUShaderStage.FRAGMENT,
+            sampler: {}
+        }, {
+            binding: 1,
+            visibility: GPUShaderStage.FRAGMENT,
+            texture: {}
+        }]
+    });
+
+    const uniformBindGroup = device.createBindGroup({
+        layout: bindGroupLayout,
+        entries: [{
+        binding: 0,
+        resource: sampler,
+        }, {
+        binding: 1,
+        resource: texture.createView(),
+        }],
+    });
+
+    const pipelineLayout = device.createPipelineLayout({ bindGroupLayouts: [bindGroupLayout] });
+
+    const pipeline = device.createRenderPipeline({
+        layout: pipelineLayout,
+        vertex: {
+            module: device.createShaderModule({
+                code: vs
+            }),
+            entryPoint: 'main',
+            buffers: [{
+                arrayStride: 7 * 4,
+                attributes:[{
+                    shaderLocation: 0,
+                    offset: 0,
+                    format: 'float32x2'
+                },{
+                    shaderLocation: 1,
+                    offset: 2 * 4,
+                    format: 'float32x3'
+                },{
+                    shaderLocation: 2,
+                    offset: 5 * 4,
+                    format: 'float32x2'
+                }]
+            }]
+        },
+        fragment: {
+            module: device.createShaderModule({
+                code: fs
+            }),
+            entryPoint: 'main',
+            targets: [{
+                format: swapChainFormat
+            }]
+        },
+        primitive: {
+            topology: 'triangle-strip',
+            stripIndexFormat:'uint16',
+        },
+    });
+
+    const renderPassDescriptor = {
+        colorAttachments: [{
+            view: null,
+            loadOp: 'clear',
+            storeOp: 'store',
+            clearValue: {
+                r: 1,
+                g: 1,
+                b: 1,
+                a: 1,
+            }
+        }],
+    };    
+
+    function render() {
+        renderPassDescriptor.colorAttachments[0].view = context.getCurrentTexture().createView();
+        
+        const commandEncoder = device.createCommandEncoder({});
+        const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+        passEncoder.setPipeline(pipeline);
+        passEncoder.setBindGroup(0, uniformBindGroup);
+        passEncoder.setVertexBuffer(0, verticesBuffer);
+        passEncoder.draw(4, 1, 0, 0);
+        passEncoder.end();
+
+        device.queue.submit([commandEncoder.finish()]);
+    }
+
+    render();
+}
+
+initWebGPU();
